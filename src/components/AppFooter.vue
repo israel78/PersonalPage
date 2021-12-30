80f80ce4397bd974acc65441bf316b5c7922cba6
d <aimorbar@gmail.com>
2021-12-30 11:25:24 +0100

    subida cambios nombres en web principal

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,20 +1,27 @@
 <template>
   <CFooter>
     <div>
-      <a href="https://coreui.io" target="_blank">CoreUI</a>
       <span class="ms-1"
-        >&copy; {{ new Date().getFullYear() }} creativeLabs.</span
+        >&copy; {{ new Date().getFullYear() }} Competencias y experiencia
+        profesional de Adolfo Israel Moreno</span
       >
     </div>
     <div class="ms-auto">
-      <span class="me-1" target="_blank">Powered by</span>
-      <a href="https://coreui.io/vue">CoreUI for Vue</a>
+      <span class="me-1" target="_blank"></span>
+      <a href="https://coreui.io/vue"
+        >Perfil de Linkedin <CIcon :icon="icon.cibLinkedinIn" size="xxl"
+      /></a>
     </div>
   </CFooter>
 </template>
 
 <script>
+import * as icon from '@coreui/icons'
 export default {
   name: 'AppFooter',
+  setup() {
+    // count sera devuelto al template
+    return { icon }
+  },
 }
 </script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,30 +1,32 @@
 <template>
   <div>
-    <WidgetsStatsA />
     <CRow>
       <CCol :md="12">
+        <CCard>
+          <CCardHeader>Interesante</CCardHeader>
+          <CCardBody>
+            <blockquote class="blockquote mb-0">
+              <p>
+                <i>
+                  Es mas importante el grado de vocacion que se tenga en la
+                  profesión, las ganas de implicarse, ,de aportar y aprender que
+                  los conocimientos que tengas en el momento.
+                </i>
+              </p>
+            </blockquote>
+          </CCardBody>
+        </CCard>
+
         <CCard class="mb-4">
           <CCardBody>
             <CRow>
-              <CCol :sm="5">
-                <h4 id="traffic" class="card-title mb-0">Traffic</h4>
-                <div class="small text-medium-emphasis">January 2021</div>
-              </CCol>
-              <CCol :sm="7" class="d-none d-md-block">
-                <CButton color="primary" class="float-end">
-                  <CIcon icon="cil-cloud-download" />
-                </CButton>
-                <CButtonGroup
-                  class="float-end me-3"
-                  role="group"
-                  aria-label="Basic outlined example"
-                >
-                  <CButton color="secondary" variant="outline">Day</CButton>
-                  <CButton color="secondary" variant="outline" active
-                    >Month</CButton
-                  >
-                  <CButton color="secondary" variant="outline">Year</CButton>
-                </CButtonGroup>
+              <CCol :sm="9">
+                <h4 id="traffic" class="card-title mb-0">
+                  Porcentaje de trabajo Back/Front, otras funciones
+                </h4>
+                <div class="small text-medium-emphasis">
+                  2014-{{ thisYear }}
+                </div>
               </CCol>
             </CRow>
             <CRow>
@@ -36,232 +38,56 @@
           <CCardFooter>
             <CRow :xs="{ cols: 1 }" :md="{ cols: 5 }" class="text-center">
               <CCol class="mb-sm-2 mb-0">
-                <div class="text-medium-emphasis">Visits</div>
-                <strong>29.703 Users (40%)</strong>
+                <div class="text-medium-emphasis">Java</div>
+                <strong>Spring / Struts ORM´s ...</strong>
                 <CProgress
                   class="mt-2"
                   color="success"
                   thin
                   :precision="1"
-                  :value="40"
+                  :value="100"
                 />
               </CCol>
               <CCol class="mb-sm-2 mb-0 d-md-down-none">
-                <div class="text-medium-emphasis">Unique</div>
-                <strong>24.093 Users (20%)</strong>
+                <div class="text-medium-emphasis">HTML/CSS/javascript</div>
+                <strong>Vue..(empezando)</strong>
                 <CProgress
                   class="mt-2"
                   color="info"
                   thin
                   :precision="1"
-                  :value="20"
+                  :value="100"
                 />
               </CCol>
               <CCol class="mb-sm-2 mb-0">
-                <div class="text-medium-emphasis">Pageviews</div>
-                <strong>78.706 Views (60%)</strong>
+                <div class="text-medium-emphasis">Bases de Datos</div>
+                <strong>Oracle/MySQL..</strong>
                 <CProgress
                   class="mt-2"
                   color="warning"
                   thin
                   :precision="1"
-                  :value="60"
+                  :value="100"
                 />
               </CCol>
               <CCol class="mb-sm-2 mb-0">
-                <div class="text-medium-emphasis">New Users</div>
-                <strong>22.123 Users (80%)</strong>
+                <div class="text-medium-emphasis">Servidores/nube</div>
+                <strong>Tomcat/AWS..</strong>
                 <CProgress
                   class="mt-2"
                   color="danger"
                   thin
                   :precision="1"
-                  :value="80"
+                  :value="100"
                 />
               </CCol>
               <CCol class="mb-sm-2 mb-0 d-md-down-none">
-                <div class="text-medium-emphasis">Bounce Rate</div>
-                <strong>Average Rate (40.15%)</strong>
-                <CProgress class="mt-2" :value="40" thin :precision="1" />
+                <div class="text-medium-emphasis">Otros</div>
+                <strong>Git/Svn, Scrum..</strong>
+                <CProgress class="mt-2" :value="100" thin :precision="1" />
               </CCol>
             </CRow>
           </CCardFooter>
-        </CCard>
-      </CCol>
-    </CRow>
-    <WidgetsStatsD />
-    <CRow>
-      <CCol :md="12">
-        <CCard class="mb-4">
-          <CCardHeader> Traffic &amp; Sales </CCardHeader>
-          <CCardBody>
-            <CRow>
-              <CCol :sm="12" :lg="6">
-                <CRow>
-                  <CCol :sm="6">
-                    <div
-                      class="border-start border-start-4 border-start-info py-1 px-3 mb-3"
-                    >
-                      <div class="text-medium-emphasis small">New Clients</div>
-                      <div class="fs-5 fw-semibold">9,123</div>
-                    </div>
-                  </CCol>
-                  <CCol :sm="6">
-                    <div
-                      class="border-start border-start-4 border-start-danger py-1 px-3 mb-3"
-                    >
-                      <div class="text-medium-emphasis small">
-                        Recurring Clients
-                      </div>
-                      <div class="fs-5 fw-semibold">22,643</div>
-                    </div>
-                  </CCol>
-                </CRow>
-                <hr class="mt-0" />
-                <div
-                  v-for="item in progressGroupExample1"
-                  :key="item.title"
-                  class="progress-group mb-4"
-                >
-                  <div class="progress-group-prepend">
-                    <span class="text-medium-emphasis small">{{
-                      item.title
-                    }}</span>
-                  </div>
-                  <div class="progress-group-bars">
-                    <CProgress thin color="info" :value="item.value1" />
-                    <CProgress thin color="danger" :value="item.value2" />
-                  </div>
-                </div>
-              </CCol>
-              <CCol :sm="12" :lg="6">
-                <CRow>
-                  <CCol :sm="6">
-                    <div
-                      class="border-start border-start-4 border-start-warning py-1 px-3 mb-3"
-                    >
-                      <div class="text-medium-emphasis small">Pageviews</div>
-                      <div class="fs-5 fw-semibold">78,623</div>
-                    </div>
-                  </CCol>
-                  <CCol :sm="6">
-                    <div
-                      class="border-start border-start-4 border-start-success py-1 px-3 mb-3"
-                    >
-                      <div class="text-medium-emphasis small">Organic</div>
-                      <div class="fs-5 fw-semibold">49,123</div>
-                    </div>
-                  </CCol>
-                </CRow>
-                <hr class="mt-0" />
-                <div
-                  v-for="item in progressGroupExample2"
-                  :key="item.title"
-                  class="progress-group"
-                >
-                  <div class="progress-group-header">
-                    <CIcon :icon="item.icon" class="me-2" size="lg" />
-                    <span class="title">{{ item.title }}</span>
-                    <span class="ms-auto fw-semibold">{{ item.value }}%</span>
-                  </div>
-                  <div class="progress-group-bars">
-                    <CProgress thin :value="item.value" color="warning" />
-                  </div>
-                </div>
-
-                <div class="mb-5"></div>
-
-                <div
-                  v-for="item in progressGroupExample3"
-                  :key="item.title"
-                  class="progress-group"
-                >
-                  <div class="progress-group-header">
-                    <CIcon :icon="item.icon" class="me-2" size="lg" />
-                    <span class="title">Organic Search</span>
-                    <span class="ms-auto fw-semibold">
-                      {{ item.value }}
-                      <span class="text-medium-emphasis small"
-                        >({{ item.percent }}%)</span
-                      >
-                    </span>
-                  </div>
-                  <div class="progress-group-bars">
-                    <CProgress thin :value="item.percent" color="success" />
-                  </div>
-                </div>
-              </CCol>
-            </CRow>
-            <br />
-            <CTable align="middle" class="mb-0 border" hover responsive>
-              <CTableHead color="light">
-                <CTableRow>
-                  <CTableHeaderCell class="text-center">
-                    <CIcon name="cil-people" />
-                  </CTableHeaderCell>
-                  <CTableHeaderCell>User</CTableHeaderCell>
-                  <CTableHeaderCell class="text-center"
-                    >Country</CTableHeaderCell
-                  >
-                  <CTableHeaderCell>Usage</CTableHeaderCell>
-                  <CTableHeaderCell class="text-center"
-                    >Payment Method</CTableHeaderCell
-                  >
-                  <CTableHeaderCell>Activity</CTableHeaderCell>
-                </CTableRow>
-              </CTableHead>
-              <CTableBody>
-                <CTableRow v-for="item in tableExample" :key="item.name">
-                  <CTableDataCell class="text-center">
-                    <CAvatar
-                      size="md"
-                      :src="item.avatar.src"
-                      :status="item.avatar.status"
-                    />
-                  </CTableDataCell>
-                  <CTableDataCell>
-                    <div>{{ item.user.name }}</div>
-                    <div class="small text-medium-emphasis">
-                      <span>{{ item.user.new ? 'New' : 'Recurring' }}</span> |
-                      {{ item.user.registered }}
-                    </div>
-                  </CTableDataCell>
-                  <CTableDataCell class="text-center">
-                    <CIcon
-                      size="xl"
-                      :name="item.country.flag"
-                      :title="item.country.name"
-                    />
-                  </CTableDataCell>
-                  <CTableDataCell>
-                    <div class="clearfix">
-                      <div class="float-start">
-                        <strong>{{ item.usage.value }}%</strong>
-                      </div>
-                      <div class="float-end">
-                        <small class="text-medium-emphasis">
-                          {{ item.usage.period }}
-                        </small>
-                      </div>
-                    </div>
-                    <CProgress
-                      thin
-                      :color="item.usage.color"
-                      :value="item.usage.value"
-                    />
-                  </CTableDataCell>
-                  <CTableDataCell class="text-center">
-                    <CIcon size="xl" :name="item.payment.icon" />
-                  </CTableDataCell>
-                  <CTableDataCell>
-                    <div class="small text-medium-emphasis">Last login</div>
-                    <strong>{{ item.activity }}</strong>
-                  </CTableDataCell>
-                </CTableRow>
-                <CTableRow> </CTableRow>
-              </CTableBody>
-            </CTable>
-          </CCardBody>
         </CCard>
       </CCol>
     </CRow>
@@ -276,15 +102,10 @@ import avatar4 from '@/assets/images/avatars/4.jpg'
 import avatar5 from '@/assets/images/avatars/5.jpg'
 import avatar6 from '@/assets/images/avatars/6.jpg'
 import MainChartExample from './charts/MainChartExample'
-import WidgetsStatsA from './widgets/WidgetsStatsTypeA.vue'
-import WidgetsStatsD from './widgets/WidgetsStatsTypeD.vue'
-
 export default {
   name: 'Dashboard',
   components: {
     MainChartExample,
-    WidgetsStatsA,
-    WidgetsStatsD,
   },
   setup() {
     const progressGroupExample1 = [
@@ -401,12 +222,15 @@ export default {
         activity: 'Last week',
       },
     ]
+    let date = new Date()
+    const thisYear = date.getFullYear()
 
     return {
       tableExample,
       progressGroupExample1,
       progressGroupExample2,
       progressGroupExample3,
+      thisYear,
     }
   },
 }
